171354801b58b0ea5bc37df6a2d9f3e4d24dce75
Torin-Perkins <tdmp42@gmail.com>
2024-09-21 10:53:05 -0700

    Add project files.

---
+++ 2Read/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using _2Read.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2Read.Controllers
+{
+	public class BookController : Controller
+	{
+
+		private readonly ApplicationDbContext _db;
+
+		public BookController(ApplicationDbContext db)
+		{
+			_db = db; 
+		}
+
+		// GET: Books
+		public IActionResult Index()
+		{
+			return View(_db.Books.ToList());
+		}
+
+		//GET: Books/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
+		// POST: Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create([Bind("Id, Name, Author")] Book book)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Add(book);
+				_db.SaveChanges();
+
+				return RedirectToAction(nameof(Index));
+			}
+
+			return View(book);
+		}
+	}
+}
---
+++ 2Read/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _2Read.Models
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+		{ 
+		}
+		public DbSet<Book> Books { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseSqlite("Filename=./books.db");
+		}
+	}
+}
